ce62c6c1e98cc9a62f21547c67faabcef5f1bd89
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-12-02 10:21:46 +0400

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,14 +6,13 @@
  * @value: value in new node
  * Return: The returning node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
 
 new_node = malloc(sizeof(binary_tree_t));
 if(!new_node)
-        return (NULL);
+    return (NULL);
 
 new_node->n = value;
 new_node->parent = parent;
