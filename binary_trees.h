d7b2487b090fd113d0443d50a372fda1f827f80b
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-12-04 10:38:22 +0400

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -47,6 +47,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int custom_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 int get_power(int exp);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
